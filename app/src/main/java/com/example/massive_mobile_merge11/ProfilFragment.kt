2e7cdbb75d0c6b4653e10da6872da2fb5321f21b
Dhicahya <leyasu45153@gmail.com>
2023-12-05 12:45:19 +0700

    activate button

--- app/src/main/java/com/example/massive_mobile_merge11/ProfilFragment.kt
+++ app/src/main/java/com/example/massive_mobile_merge11/ProfilFragment.kt
@@ -1,10 +1,12 @@
 package com.example.massive_mobile_merge11
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.massive_mobile_merge11.databinding.FragmentProfilBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,8 @@ class ProfilFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentProfilBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +39,19 @@ class ProfilFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false)
+        _binding = FragmentProfilBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.btnEdit.setOnClickListener{
+            val intent = Intent(activity, EditProfilActivity::class.java)
+            startActivity(intent)
+        }
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
